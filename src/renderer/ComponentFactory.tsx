b1fb2b1185bce4b553c4e236229e567ddb848bde
gokulm <gokulnathmurugesan@gmail.com>
2021-12-05 17:55:16 -0500

    added component factory

--- src/renderer/ComponentFactory.tsx
+++ src/renderer/ComponentFactory.tsx
@@ -1,33 +1,40 @@
 import { Form } from "react-bootstrap";
-import { useFormContext } from "react-hook-form";
+import { FieldValue, FieldValues, useFormContext, UseFormRegister } from "react-hook-form";
 import { IComponentAttribute } from "./JsonRenderer"
 
-const ComponentFactory = (props: any) => {
-    const { register } = useFormContext();
-    
-    const componentMappers: { [key: string]: Function } = {
-        "flex": (componentProps: any) => renderFlexContainer(componentProps),
-        "text": (componentProps: any) => renderTextBox(componentProps),
-        "dropdown": (componentProps: any) => renderDropDown(componentProps)
+export class ComponentFactory {
+
+    private _componentMapper: { [key: string]: Function } = {}
+
+    constructor(private _register: UseFormRegister<FieldValues>) {
+        this._componentMapper = {
+            "flex": (componentProps: any) => this.renderFlexContainer(componentProps),
+            "text": (componentProps: any) => this.renderTextBox(componentProps),
+            "dropdown": (componentProps: any) => this.renderDropDown(componentProps)
+        }
     }
 
-    const renderFlexContainer = (componentProps: IComponentAttribute) => {
+    private renderFlexContainer(componentProps: IComponentAttribute) {
         return <div className="renderer-flex-container" style={componentProps.style}>
-            {componentProps.children.map(c => <div> {componentMappers[c.type](c)} </div>)}</div>
+            {componentProps.children.map(c => <div> {this._componentMapper[c.type](c)} </div>)}</div>
     }
 
-    const renderTextBox = (componentProps: IComponentAttribute) => {
+    private renderTextBox(componentProps: IComponentAttribute) {
         return <input type="text" placeholder={componentProps.placeHolder}
-            {...register(componentProps.name, { required: false })}
+            {...this._register(componentProps.name, { required: false })}
             defaultValue={componentProps.defaultValue} />
     }
 
-    const renderDropDown = (componentProps: IComponentAttribute) => {
-        return <Form.Select   {...register(componentProps.name, { required: false })} >
+    private renderDropDown(componentProps: IComponentAttribute) {
+        return <Form.Select   {...this._register(componentProps.name, { required: false })} >
             <option value={undefined}>{componentProps.placeHolder}</option>
             {componentProps.dropdownValues?.map(d => <option value={d}>{d}</option>)}
         </Form.Select>
     }
+
+    public render(componentProps: IComponentAttribute) {
+        return this._componentMapper[componentProps.type](componentProps);
+    }
 }
 
-export default ComponentFactory;
+// export default ComponentFactory;
--- src/renderer/JsonRenderer.tsx
+++ src/renderer/JsonRenderer.tsx
@@ -1,5 +1,5 @@
-import { Form } from 'react-bootstrap';
 import { useFormContext } from 'react-hook-form'
+import { ComponentFactory } from './ComponentFactory';
 import './JsonRenderer.scss'
 
 interface IProps {
@@ -21,35 +21,37 @@ export interface IComponentAttribute {
 
 const JsonRenderer = (props: IProps) => {
     const { register } = useFormContext();
+    const componentFactory = new ComponentFactory(register);
 
-    const componentMappers: { [key: string]: Function } = {
-        "flex": (componentProps: any) => renderFlexContainer(componentProps),
-        "text": (componentProps: any) => renderTextBox(componentProps),
-        "dropdown": (componentProps: any) => renderDropDown(componentProps)
-    }
+    // const componentMappers: { [key: string]: Function } = {
+    //     "flex": (componentProps: any) => renderFlexContainer(componentProps),
+    //     "text": (componentProps: any) => renderTextBox(componentProps),
+    //     "dropdown": (componentProps: any) => renderDropDown(componentProps)
+    // }
 
-    const renderFlexContainer = (componentProps: IComponentAttribute) => {
-        return <div className="renderer-flex-container" style={componentProps.style}>
-            {componentProps.children.map(c => <div> {componentMappers[c.type](c)} </div>)}</div>
-    }
+    // const renderFlexContainer = (componentProps: IComponentAttribute) => {
+    //     return <div className="renderer-flex-container" style={componentProps.style}>
+    //         {componentProps.children.map(c => <div> {componentMappers[c.type](c)} </div>)}</div>
+    // }
 
-    const renderTextBox = (componentProps: IComponentAttribute) => {
-        return <input type="text" placeholder={componentProps.placeHolder}
-            {...register(componentProps.name, { required: false })}
-            defaultValue={componentProps.defaultValue} />
-    }
+    // const renderTextBox = (componentProps: IComponentAttribute) => {
+    //     return <input type="text" placeholder={componentProps.placeHolder}
+    //         {...register(componentProps.name, { required: false })}
+    //         defaultValue={componentProps.defaultValue} />
+    // }
 
-    const renderDropDown = (componentProps: IComponentAttribute) => {
-        return <Form.Select   {...register(componentProps.name, { required: false })} >
-            <option value={undefined}>{componentProps.placeHolder}</option>
-            {componentProps.dropdownValues?.map(d => <option value={d}>{d}</option>)}
-        </Form.Select>
-    }
+    // const renderDropDown = (componentProps: IComponentAttribute) => {
+    //     return <Form.Select   {...register(componentProps.name, { required: false })} >
+    //         <option value={undefined}>{componentProps.placeHolder}</option>
+    //         {componentProps.dropdownValues?.map(d => <option value={d}>{d}</option>)}
+    //     </Form.Select>
+    // }
 
     return (
         <>
             {
-                (props.schema).map(m => componentMappers[m.type](m))
+                // (props.schema).map(m => componentMappers[m.type](m))
+                (props.schema).map(m => componentFactory.render(m))
             }
         </>
     );
